cee087cbaf7516b317f1ddb0c2db8d7869c6e69c
Matúš Tomlein <matus.tomlein@gmail.com>
2022-02-16 11:26:05 +0100

    Add include for mutex

--- src/client_session.hpp
+++ src/client_session.hpp
@@ -15,6 +15,7 @@ See the Apache License Version 2.0 for the specific language governing permissio
 #define CLIENT_SESSION_H
 
 #include <string>
+#include <mutex>
 #include "self_describing_json.hpp"
 #include "../include/json.hpp"
 
