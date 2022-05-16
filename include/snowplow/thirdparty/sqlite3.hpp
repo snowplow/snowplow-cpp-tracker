d98e16bc7533a17b187be8024f0357aab64f283e
Matúš Tomlein <matus.tomlein@gmail.com>
2022-05-16 08:47:53 +0200

    Update include instructions for sqlite3 and json

--- include/snowplow/thirdparty/sqlite3.hpp
+++ include/snowplow/thirdparty/sqlite3.hpp
@@ -1,8 +1,21 @@
-// Replace the following with the path the SQLite3 library
+/*
+Copyright (c) 2022 Snowplow Analytics Ltd. All rights reserved.
+
+This program is licensed to you under the Apache License Version 2.0,
+and you may not use this file except in compliance with the Apache License Version 2.0.
+You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the Apache License Version 2.0 is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+*/
 
 #ifndef INCLUDE_SQLITE_H
 #define INCLUDE_SQLITE_H
 
+// Replace the following with the path the SQLite3 library
+// You may download the amalgamated version from https://www.sqlite.org/download.html
 #include "sqlite3.h"
 
 #endif
